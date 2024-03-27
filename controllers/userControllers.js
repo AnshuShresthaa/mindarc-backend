f746beee29bea382a391f6e0fdeb938eb45f5caf
sthaanshuu <anshushrestha9@gmail.com>
2024-03-27 10:04:11 +0545

    User Management

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,6 @@
 import { uploadPicture } from "../middleware/uploadPictureMiddleware";
+import Comment from "../models/Comment";
+import Post from "../models/Post";
 import User from "../models/User";
 import { fileRemover } from "../utils/fileRemover";
 
@@ -87,10 +89,24 @@ const userProfile = async (req, res, next)=>{
 
 const updateProfile = async (req, res, next) => {
     try{
-      let user = await User.findById(req.user._id);
+      const userIdToUpdate = req.params.userId;
+
+      let userId = req.user._id;
+
+      if (!req.user.admin && userId !== userIdToUpdate) {
+        let error = new Error("Forbidden resource");
+        error.statusCode = 403;
+        throw error;
+      }
+
+      let user = await User.findById(userIdToUpdate);
 
       if(!user){
         throw new Error("{User not found");
+      }
+
+      if (typeof req.body.admin !== "undefined" && req.user.admin) {
+        user.admin = req.body.admin;
       }
 
       user.name = req.body.name || user.name;
@@ -166,7 +182,76 @@ const updateProfilePicture = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+const getAllUsers = async (req, res, next) => {
+    try {
+      const filter = req.query.searchKeyword;
+      let where = {};
+      if (filter) {
+        where.email = { $regex: filter, $options: "i" };
+      }
+      let query = User.find(where);
+      const page = parseInt(req.query.page) || 1;
+      const pageSize = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * pageSize;
+      const total = await User.find(where).countDocuments();
+      const pages = Math.ceil(total / pageSize);
+  
+      res.header({
+        "x-filter": filter,
+        "x-totalcount": JSON.stringify(total),
+        "x-currentpage": JSON.stringify(page),
+        "x-pagesize": JSON.stringify(pageSize),
+        "x-totalpagecount": JSON.stringify(pages),
+      });
+  
+      if (page > pages) {
+        return res.json([]);
+      }
+  
+      const result = await query
+        .skip(skip)
+        .limit(pageSize)
+        .sort({ updatedAt: "desc" });
+  
+      return res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  const deleteUser = async (req, res, next) => {
+    try {
+      let user = await User.findById(req.params.userId);
+  
+      if (!user) {
+        throw new Error("User no found");
+      }
+  
+      const postsToDelete = await Post.find({ user: user._id });
+      const postIdsToDelete = postsToDelete.map((post) => post._id);
+  
+      await Comment.deleteMany({
+        post: { $in: postIdsToDelete },
+      });
+  
+      await Post.deleteMany({
+        _id: { $in: postIdsToDelete },
+      });
+  
+      postsToDelete.forEach((post) => {
+        fileRemover(post.photo);
+      });
+
+      await user.remove();
+      fileRemover(user.avatar);
+
+      res.status(204).json({ message: "User is deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  };
 
 export { 
     registerUser, 
@@ -174,4 +259,6 @@ export {
     userProfile, 
     updateProfile,
     updateProfilePicture,
+    getAllUsers,
+    deleteUser,
  };
--- server.js
+++ server.js
@@ -38,4 +38,5 @@ app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
